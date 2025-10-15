2b962358e46f5c67e5664e561d20914f349f904e
OlenaKirieva <Alenushka2013@i.ua>
2025-10-15 13:49:17 +0300

    Update vectors.py
    All methods are implemented

--- vectors.py
+++ vectors.py
@@ -13,10 +13,10 @@ def get_vector(dim: int) -> np.ndarray:
     Returns:
         np.ndarray: column vector.
     """
-    raise NotImplementedError
+    return np.random.rand(dim, 1)
 
 
-def get_sparse_vector(dim: int) -> sparse.coo_matrix:
+def get_sparse_vector(dim: int, density: float = 0.5) -> sparse.coo_matrix: 
     """Create random sparse column vector with dimension dim.
 
     Args:
@@ -25,7 +25,14 @@ def get_sparse_vector(dim: int) -> sparse.coo_matrix:
     Returns:
         sparse.coo_matrix: sparse column vector.
     """
-    raise NotImplementedError
+    data = np.random.rand(dim)
+    mask = np.random.rand(dim) < density
+    data[mask] = 0
+    
+    row = np.arange(dim)
+    col = np.zeros(dim)
+    
+    return sparse.coo_matrix((data, (row, col)), shape=(dim, 1))
 
 
 def add(x: np.ndarray, y: np.ndarray) -> np.ndarray:
@@ -38,7 +45,7 @@ def add(x: np.ndarray, y: np.ndarray) -> np.ndarray:
     Returns:
         np.ndarray: vector sum.
     """
-    raise NotImplementedError
+    return x + y
 
 
 def scalar_multiplication(x: np.ndarray, a: float) -> np.ndarray:
@@ -51,7 +58,7 @@ def scalar_multiplication(x: np.ndarray, a: float) -> np.ndarray:
     Returns:
         np.ndarray: multiplied vector.
     """
-    raise NotImplementedError
+    return a * x 
 
 
 def linear_combination(vectors: Sequence[np.ndarray], coeffs: Sequence[float]) -> np.ndarray:
@@ -64,7 +71,14 @@ def linear_combination(vectors: Sequence[np.ndarray], coeffs: Sequence[float]) -
     Returns:
         np.ndarray: linear combination of vectors.
     """
-    raise NotImplementedError
+    if len(vectors) != len(coeffs):
+      raise ValueError("The number of vectors does not match the number of coefficients.")
+      
+    result = np.zeros_like(vectors[0])
+    for v, c in zip(vectors, coeffs):
+        result += c * v
+        
+    return result
 
 
 def dot_product(x: np.ndarray, y: np.ndarray) -> float:
@@ -77,7 +91,7 @@ def dot_product(x: np.ndarray, y: np.ndarray) -> float:
     Returns:
         float: dot product.
     """
-    raise NotImplementedError
+    return float(np.dot(x, y))
 
 
 def norm(x: np.ndarray, order: int | float) -> float:
@@ -90,7 +104,20 @@ def norm(x: np.ndarray, order: int | float) -> float:
     Returns:
         float: vector norm
     """
-    raise NotImplementedError
+    if order == 1:
+        #  Manhattan: |x1| + |x2| + ... + |xn|
+        return float(np.sum(np.abs(x)))
+    
+    elif order == 2:
+        # Euclidean: sqrt(x1² + x2² + ... + xn²)
+        return float(np.sqrt(np.sum(x ** 2)))
+    
+    elif order == np.inf:
+        # Max: max(|x1|, |x2|, ..., |xn|)
+        return float(np.max(np.abs(x)))
+    
+    else:
+        raise ValueError("Only norms of order 1, 2, or np.inf are supported")
 
 
 def distance(x: np.ndarray, y: np.ndarray) -> float:
@@ -103,7 +130,7 @@ def distance(x: np.ndarray, y: np.ndarray) -> float:
     Returns:
         float: distance.
     """
-    raise NotImplementedError
+    return float(sum((x-y) ** 2)) ** 0.5
 
 
 def cos_between_vectors(x: np.ndarray, y: np.ndarray) -> float:
@@ -117,8 +144,11 @@ def cos_between_vectors(x: np.ndarray, y: np.ndarray) -> float:
     Returns:
         np.ndarray: angle in deg.
     """
-    raise NotImplementedError
-
+    
+    cos_theta = dot_product(x, y) / (norm(x, 2) * norm(y, 2))
+    angle_rad = np.arccos(cos_theta)
+    
+    return float(np.degrees(angle_rad))
 
 def is_orthogonal(x: np.ndarray, y: np.ndarray) -> bool:
     """Check is vectors orthogonal.
@@ -131,7 +161,7 @@ def is_orthogonal(x: np.ndarray, y: np.ndarray) -> bool:
     Returns:
         bool: are vectors orthogonal.
     """
-    raise NotImplementedError
+    return float(np.dot(x, y)) == 0
 
 
 def solves_linear_systems(a: np.ndarray, b: np.ndarray) -> np.ndarray:
@@ -144,4 +174,17 @@ def solves_linear_systems(a: np.ndarray, b: np.ndarray) -> np.ndarray:
     Returns:
         np.ndarray: sytems solution
     """
-    raise NotImplementedError
+    if a.shape[0] != a.shape[1]:
+        raise ValueError("Matrix 'a' must be square")
+
+    if a.shape[0] != b.shape[0]:
+        raise ValueError("Matrix 'a' and vector 'b' dimensions do not match")
+
+    det = np.linalg.det(a)
+    if np.isclose(det, 0.0):
+        raise ValueError("Matrix 'a' is singular — the system has no unique solution")
+
+    a_inv = np.linalg.inv(a)
+    x = a_inv.dot(b)
+
+    return x
